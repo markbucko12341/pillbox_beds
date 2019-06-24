b7ad314f0d362a906be0dfdebcefb8c2ed9d2fdb
SuperiorNetwork <edwardsjames@live.com>
2019-06-24 15:30:21 -0400

    Cleanup

--- pillbox_beds/client.lua
+++ pillbox_beds/client.lua
@@ -1,20 +1,11 @@
-ESX = nil
-local InAction = false
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-end)
-
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(5)
         for i=1, #Config.BedList do
-            local bedID   = Config.BedList[i]
+            local bedID = Config.BedList[i]
             local distance = GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), bedID.objCoords.x, bedID.objCoords.y, bedID.objCoords.z, true)
             if distance < Config.MaxDistance and InAction == false then
-                ESX.Game.Utils.DrawText3D({ x = bedID.objCoords.x, y = bedID.objCoords.y, z = bedID.objCoords.z + 1 }, 'Press [E] to lay down', 0.6)
+                headsUp('Press ~INPUT_CONTEXT~ to lay down')
                 if IsControlJustReleased(0, 38) then
                     bedActive(bedID.objCoords.x, bedID.objCoords.y, bedID.objCoords.z, bedID.heading, bedID)
                 end
@@ -29,7 +20,7 @@ function bedActive(x, y, z, heading)
     while not HasAnimDictLoaded('anim@gangops@morgue@table@') do
         Citizen.Wait(0)
     end
-    TaskPlayAnim(GetPlayerPed(-1), 'anim@gangops@morgue@table@' , 'ko_front' ,8.0, -8.0, -1, 1, 0, false, false, false )
+    TaskPlayAnim(GetPlayerPed(-1), 'anim@gangops@morgue@table@', 'ko_front', 8.0, -8.0, -1, 1, 0, false, false, false)
     SetEntityHeading(GetPlayerPed(-1), heading + 180.0)
     InAction = true
     Citizen.CreateThread(function()
